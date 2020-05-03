c7970d94aebcf84cb44be5c272becf100886a034
Poorna Chandra Tejasvi <pct960@gmail.com>
2020-05-03 17:00:01 +0530

    Decoding geohash to lat long

--- process.go
+++ process.go
@@ -3,28 +3,33 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"github.com/mmcloughlin/geohash"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
-	"github.com/mmcloughlin/geohash"
 )
 
 const (
-    MIN_COUNT_OF_PEOPLE	= 10
-    SMALL_GRID_BITS = 7 //76m grid
-    MEDIUM_GRID_BITS = 6 //610m grid
-    LARGE_GRID_BITS = 5 //2.4km grid
-    VERY_LARGE_GRID_BITS = 4 //20km grid
-    LARGEST_GRID_BITS = 3 //78km grid
+	MIN_COUNT_OF_PEOPLE  = 10
+	SMALL_GRID_BITS      = 7 //76m grid
+	MEDIUM_GRID_BITS     = 6 //610m grid
+	LARGE_GRID_BITS      = 5 //2.4km grid
+	VERY_LARGE_GRID_BITS = 4 //20km grid
+	LARGEST_GRID_BITS    = 3 //78km grid
 )
 
 var rows [][]string
 
-func truncate(lat, lon float64, bits int) string {
-    str:= geohash.Encode(lat, lon)
-    return str[:bits+1]
+func encode(lat, lon float64, bits int) string {
+	str := geohash.Encode(lat, lon)
+	return str[:bits+1]
+}
+
+func truncate(x float64, n float64) float64 {
+	return math.Floor(x*math.Pow(10, n)) / math.Pow(10, n)
 }
 
 func RemoveIndex(s []string, index int) []string {
@@ -81,13 +86,13 @@ func main() {
 		lon, _ := strconv.ParseFloat(lon_str, 64)
 		created_on := strings.Split(created_on_str, " ")[0]
 
-		smallGridHash := truncate(lat, lon, SMALL_GRID_BITS)
-		mediumGridHash := truncate(lat, lon, MEDIUM_GRID_BITS)
-		largeGridHash := truncate(lat, lon, LARGE_GRID_BITS)
-		veryLargeGridHash := truncate(lat, lon, VERY_LARGE_GRID_BITS)
-		largestGridHash := truncate(lat, lon, LARGEST_GRID_BITS)
+		smallGridHash := encode(lat, lon, SMALL_GRID_BITS)
+		mediumGridHash := encode(lat, lon, MEDIUM_GRID_BITS)
+		largeGridHash := encode(lat, lon, LARGE_GRID_BITS)
+		veryLargeGridHash := encode(lat, lon, VERY_LARGE_GRID_BITS)
+		largestGridHash := encode(lat, lon, LARGEST_GRID_BITS)
 
-		//TODO: If files are on a per-day basis, this 
+		//TODO: If files are on a per-day basis, this
 		//overhead can be avoided
 		smallGridKey := smallGridHash + "," + created_on
 		mediumGridKey := mediumGridHash + "," + created_on
@@ -104,33 +109,33 @@ func main() {
 		largestGridKeys[lat_lon_key] = largestGridHash
 
 		if _, ok := smallGrid[smallGridKey]; ok {
-		    smallGrid[smallGridKey]+=1
+			smallGrid[smallGridKey] += 1
 		} else {
-		    smallGrid[smallGridKey] =1
+			smallGrid[smallGridKey] = 1
 		}
 
 		if _, ok := mediumGrid[mediumGridKey]; ok {
-		    mediumGrid[mediumGridKey]+=1
+			mediumGrid[mediumGridKey] += 1
 		} else {
-		    mediumGrid[mediumGridKey] =1
+			mediumGrid[mediumGridKey] = 1
 		}
 
 		if _, ok := largeGrid[largeGridKey]; ok {
-		    largeGrid[largeGridKey]+=1
+			largeGrid[largeGridKey] += 1
 		} else {
-		    largeGrid[largeGridKey] =1
+			largeGrid[largeGridKey] = 1
 		}
 
 		if _, ok := veryLargeGrid[veryLargeGridKey]; ok {
-		    veryLargeGrid[veryLargeGridKey]+=1
+			veryLargeGrid[veryLargeGridKey] += 1
 		} else {
-		    veryLargeGrid[veryLargeGridKey] =1
+			veryLargeGrid[veryLargeGridKey] = 1
 		}
 
 		if _, ok := largestGrid[largestGridKey]; ok {
-		    largestGrid[largestGridKey]+=1
+			largestGrid[largestGridKey] += 1
 		} else {
-		    largestGrid[largestGridKey] =1
+			largestGrid[largestGridKey] = 1
 		}
 
 	}
@@ -171,12 +176,6 @@ func main() {
 		record = RemoveIndex(record, 17)
 
 		if count == 0 {
-			//Remove lat
-			record = RemoveIndex(record, 5)
-			//Remove long
-			record = RemoveIndex(record, 5)
-			//Append a new field "geohash"
-			record = append(record, "geohash")
 			rows = append(rows, record)
 			count += 1
 			continue
@@ -192,30 +191,37 @@ func main() {
 
 		created_on := strings.Split(created_on_str, " ")[0]
 
-		lat_lon_key:= lat_str + "," + lon_str
+		lat_lon_key := lat_str + "," + lon_str
 		smallGridKey := smallGridKeys[lat_lon_key] + "," + created_on
 		mediumGridKey := mediumGridKeys[lat_lon_key] + "," + created_on
 		largeGridKey := largeGridKeys[lat_lon_key] + "," + created_on
 		veryLargeGridKey := veryLargeGridKeys[lat_lon_key] + "," + created_on
 		largestGridKey := largestGridKeys[lat_lon_key] + "," + created_on
 
-		//Remove lat fields
-		record = RemoveIndex(record, 5)
-		//Remove lon field
-		record = RemoveIndex(record, 5)
-
 		if smallGrid[smallGridKey] >= MIN_COUNT_OF_PEOPLE {
-		    record = append(record, smallGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(smallGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		} else if mediumGrid[mediumGridKey] >= MIN_COUNT_OF_PEOPLE {
-		    record = append(record, mediumGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(mediumGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		} else if largeGrid[largeGridKey] >= MIN_COUNT_OF_PEOPLE {
-		    record = append(record, largeGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(largeGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		} else if veryLargeGrid[veryLargeGridKey] >= MIN_COUNT_OF_PEOPLE {
-		    record = append(record, veryLargeGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(veryLargeGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		} else if largestGrid[largestGridKey] >= MIN_COUNT_OF_PEOPLE {
-		    record = append(record, largestGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(largestGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		} else {
-		    record = append(record, largestGridKeys[lat_lon_key])
+			decoded_lat, decoded_lon := geohash.DecodeCenter(largestGridKeys[lat_lon_key])
+			record[5] = fmt.Sprintf("%f", truncate(decoded_lat, 3))
+			record[6] = fmt.Sprintf("%f", truncate(decoded_lon, 3))
 		}
 
 		rows = append(rows, record)
